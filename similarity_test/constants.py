9b2896d7e8fa779c4557ef13b38a9dc397b8e266
max-stack <55951090+max-stack@users.noreply.github.com>
2022-03-16 04:23:12 +0000

    removing unnecessary file

--- similarity_test/constants.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-DATASET_PATH = str(Path(__file__).parent / "../dataset")
-TRAINSET_PATH = DATASET_PATH + "/trainset.json"
-TESTSET_PATH = DATASET_PATH + "/testset.json"
-VALIDSET_PATH = DATASET_PATH + "/validset.json"
